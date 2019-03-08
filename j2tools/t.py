bd6d0c7189d4194d7ff184c7ed4a15540bdf9598
vd <vd@>
2019-03-08 20:57:13 +0200

    Refactoring in t.py.

--- j2tools/t.py
+++ j2tools/t.py
@@ -2,15 +2,18 @@
 # This file is part of Periodic package.
 # Periodic is released under the MIT License (see LICENSE).
 
+from jinja2 import Environment
 
-def T_factory(env):
-    def get_T(prefix):
-        def T(template_name, **kwargs):
-            template_name = prefix + '/' + template_name
+
+def t_factory(env: Environment, t_variable: str = 'T', separator: str = '/'):
+    def get_t(prefix: str):
+        def t(template_name: str, **kwargs):
+            template_name = prefix + separator + template_name
             template = env.get_template(template_name)
-            kwargs['T'] = T
+            if t_variable not in kwargs:
+                kwargs[t_variable] = t
             return template.render(**kwargs)
 
-        return T
+        return t
 
-    return get_T
+    return get_t
